677bc9747ac907d5df7d05ba664615b83ebfbe5a
Hiroaki Nakamura <hnakamur@gmail.com>
2017-09-19 15:05:04 +0900

    Update package document

--- doc.go
+++ doc.go
@@ -1,12 +1,14 @@
 // Package sysstat provides functionalities to monitor system statistics
 // like CPU, memory, disk, network, and uptime.
 //
-// This package focuses on the performance.
+// This package focuses on the performance, low CPU and memory footprint, to give
+// least impact on system when we are monitoring.
 //
-//   * Only minimal metrics which we are actually interested in are read.
-//   * We try to minimize memory allocations during iterations of monitoring.
+// * We monitor only minimal metrics which we are actually interested in.
+// * We try to minimize memory allocations during iterations of monitoring in order to
+//   reduce load from Go runtime GC.
 //
-// Currently we achieve zero memory allocation count during interations of monitoring.
+// Currently we achieve zero memory allocation during interations of monitoring.
 // Check it out with
 //   go test -run NONE -bench . -benchmem
 //
