978b8c96d5774cf1ec13e397fe559874bbabc2e7
Hiroaki Nakamura <hnakamur@gmail.com>
2017-09-19 10:38:25 +0900

    Add LICENSE and README

---
+++ README.md
@@ -0,0 +1,19 @@
+sysstat
+=======
+
+sysstat is a Go package which provides functionalities to monitor system statistics
+like CPU, memory, disk, network, and uptime.
+
+This package focuses on the performance.
+
+* Only minimal metrics which we are actually interested in are read.
+* We try to minimize memory allocations during iterations of monitoring.
+
+Currently we achieve zero memory allocation count during interations of monitoring.
+Check it out with
+
+```
+go test -run NONE -bench . -benchmem
+```
+
+This package supports only Linux.
--- doc.go
+++ doc.go
@@ -1,12 +1,12 @@
-// Package sysstat provides readers to monitor statistics about system,
+// Package sysstat provides functionalities to monitor system statistics
 // like CPU, memory, disk, network, and uptime.
 //
 // This package focuses on the performance.
 //
-//   * Only minimal metrics which we are actually interested are read.
+//   * Only minimal metrics which we are actually interested in are read.
 //   * We try to minimize memory allocations during iterations of monitoring.
 //
-// Currently we achieve zero memory allocation count during interatoins of monitoring.
+// Currently we achieve zero memory allocation count during interations of monitoring.
 // Check it out with
 //   go test -run NONE -bench . -benchmem
 //
