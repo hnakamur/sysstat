e9fd496010fc9d0b5701ed2b950fc2a58802532a
Hiroaki Nakamura <hnakamur@gmail.com>
2017-10-02 21:28:56 +0900

    Optimize FileSystemStatReader
    $ go test -count=10 -run=NONE -bench=BenchmarkFileSystemStatReader_Read -benchmem -memprofile=fs_stat.mprof | tee fs-stat.2.log
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/sysstat
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       385 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       386 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       386 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       388 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       385 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       385 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       385 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       384 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       387 ns/op	       0 B/op	       0 allocs/op
    BenchmarkFileSystemStatReader_Read-2   	 5000000	       387 ns/op	       0 B/op	       0 allocs/op
    PASS
    ok  	github.com/hnakamur/sysstat	23.277s
    
    $ benchstat fs-stat.0.log fs-stat.2.log | tee fs-stat.0-2.log
    name                         old time/op    new time/op    delta
    FileSystemStatReader_Read-2     440ns ± 2%     386ns ± 1%   -12.26%  (p=0.000 n=10+10)
    
    name                         old alloc/op   new alloc/op   delta
    FileSystemStatReader_Read-2     2.00B ± 0%     0.00B       -100.00%  (p=0.000 n=10+10)
    
    name                         old allocs/op  new allocs/op  delta
    FileSystemStatReader_Read-2      1.00 ± 0%      0.00       -100.00%  (p=0.000 n=10+10)

--- filesystem.go
+++ filesystem.go
@@ -2,6 +2,7 @@ package sysstat
 
 import (
 	"syscall"
+	"unsafe"
 )
 
 // FileSystemStat is a statistics for a filesystem.
@@ -17,18 +18,18 @@ type FileSystemStat struct {
 // FileSystemStatReader is used for reading uptime.
 // FileSystemStatReader is not safe for concurrent accesses.
 type FileSystemStatReader struct {
-	path string
+	path []byte
 }
 
 // NewFileSystemStatReader creates a FileSystemStatReader
 func NewFileSystemStatReader(path string) *FileSystemStatReader {
-	return &FileSystemStatReader{path: path}
+	return &FileSystemStatReader{path: []byte(path)}
 }
 
 // Read reads the uptime
 func (r *FileSystemStatReader) Read(s *FileSystemStat) error {
 	var buf syscall.Statfs_t
-	err := syscall.Statfs(r.path, &buf)
+	err := statfs(r.path, &buf)
 	if err != nil {
 		return err
 	}
@@ -40,4 +41,18 @@ func (r *FileSystemStatReader) Read(s *FileSystemStat) error {
 	s.TotalINodes = buf.Files
 	s.FreeINodes = buf.Ffree
 	return nil
+}
+
+func statfs(path []byte, buf *syscall.Statfs_t) (err error) {
+	var _p0 unsafe.Pointer
+	if len(path) > 0 {
+		_p0 = unsafe.Pointer(&path[0])
+	} else {
+		_p0 = unsafe.Pointer(&_zero)
+	}
+	_, _, e1 := syscall.Syscall(syscall.SYS_STATFS, uintptr(_p0), uintptr(unsafe.Pointer(buf)), 0)
+	if e1 != 0 {
+		err = e1
+	}
+	return
 }
